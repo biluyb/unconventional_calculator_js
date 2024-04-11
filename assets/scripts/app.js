5dc0dc506cd334f3bd7203fad9ff65481111769f
biluyb <biluyb338@gmail.com>
2024-04-11 03:07:52 +0300

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,76 +2,76 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntry = []
 
-function getUserInput(){
+function getUserInput() {
   return parseInt(userInput.value);
 }
 
-function reset(){
-  erasedNumber= 0;
-  console.log("reset") ;
+function reset() {
+  erasedNumber = 0;
+  console.log("reset");
   currentResult = 0;
-   inputField(erasedNumber);
+  inputField(erasedNumber);
 }
 
-function writeOutput(operator,resultBefore,calcResult){
-    const resultDescription = `${resultBefore} ${operator} ${calcResult}`
-    outputResult(calcResult, resultDescription)
+function writeOutput(operator, resultBefore, calcResult) {
+  const resultDescription = `${resultBefore} ${operator} ${calcResult}`
+  outputResult(calcResult, resultDescription)
 }
 function writeToLog(
   calcOperation,
   prevResult,
   theNumber,
   finalResult,
-  ){
-  const addObject={
-    operation : calcOperation,
-    prevResult : prevResult,
-    number :theNumber,
-    result : finalResult,
+) {
+  const addObject = {
+    operation: calcOperation,
+    prevResult: prevResult,
+    number: theNumber,
+    result: finalResult,
   }
   logEntry.push(addObject);
   console.log(logEntry);
-  }
-
-  function calculation(calcType){
-  const enteredNumber =getUserInput();
-  const initCurrentResult= currentResult;
-  let operator;
-
-  if (calcType==="ADD"){
-  currentResult += enteredNumber;
-  operator = "+"
-   }
-   else if(calcType==="SUBTRACTION"){
-    currentResult -= enteredNumber;
-    operator = "-"
-   }
-   else if(calcType==="MULTIPLICATION"){
-    currentResult *= enteredNumber;
-    operator = "*"
-   }
-   else if(calcType==="DIVISION"){
-    currentResult /= enteredNumber;
-    operator = "/"
-   }
-
-  writeOutput(operator,initCurrentResult,currentResult);
-  writeToLog(calcType,initCurrentResult,enteredNumber,currentResult)
-  }
-
-function add(){  
- calculation("ADD");
 }
 
-function sub(){  
+function calculation(calcType) {
+  const enteredNumber = getUserInput();
+  const initCurrentResult = currentResult;
+  let operator;
+
+  if (calcType === "ADD") {
+    currentResult += enteredNumber;
+    operator = "+"
+  }
+  else if (calcType === "SUBTRACTION") {
+    currentResult -= enteredNumber;
+    operator = "-"
+  }
+  else if (calcType === "MULTIPLICATION") {
+    currentResult *= enteredNumber;
+    operator = "*"
+  }
+  else if (calcType === "DIVISION") {
+    currentResult /= enteredNumber;
+    operator = "/"
+  }
+
+  writeOutput(operator, initCurrentResult, currentResult);
+  writeToLog(calcType, initCurrentResult, enteredNumber, currentResult)
+}
+
+function add() {
+  calculation("ADD");
+}
+
+function sub() {
   calculation("SUBTRACTION");
 }
 
-function mul(){  
+function mul() {
   calculation("MULTIPLICATION");
 }
 
-function div(){  
+function div() {
   calculation("DIVISION");
 }
 
